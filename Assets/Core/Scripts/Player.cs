538faa41be642acf924f3f3f0d6ffca307b7ae02
semechka <timon.skorpion@gmail.com>
2025-12-16 13:35:09 +0300

    Collectable items

--- Assets/Core/Scripts/Player.cs
+++ Assets/Core/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,10 +6,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private int _speed = 5;
     [SerializeField] private int _jumpForce = 10;
+    [SerializeField] private int _coinsCollected = 0;
     
     private Rigidbody _rigidbody;
     private PlayerInputActions _playerInputActions;
 
+    private bool _canJump = false;
+    
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -20,8 +24,38 @@ public class Player : MonoBehaviour
 
     private void Jump_OnPerformed(InputAction.CallbackContext obj)
     {
-        _rigidbody.AddForce(Vector3.up * _jumpForce); // new Vector3(0, 1, 0);
+        if (_canJump)
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpForce); // new Vector3(0, 1, 0);
+            _canJump = false;
+        }
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Floor"))
+        {
+            _canJump = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Collectable"))
+        {
+            _coinsCollected++;
+            Destroy(other.gameObject);
+        }
+    }
+
+    // устанавливать переменную _canJump в false можно при прекращении коллизии с полом, а не при каждом прыжке
+    // private void OnCollisionExit(Collision other)
+    // {
+    //     if (other.gameObject.layer == LayerMask.NameToLayer("Floor"))
+    //     {
+    //         _canJump = false;
+    //     }
+    // }
 
     private void Update()
     {
